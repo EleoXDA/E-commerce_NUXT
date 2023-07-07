9af4d2e518f2638a9f0488cbfcafb16e4fa03138
Elbay Malik <27622683+EleoXDA@users.noreply.github.com>
2023-07-07 22:36:29 +0200

    add Error handling for wrong url or item name

---
+++ error.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <p>{{ error.statusCode }}</p>
+    <p>Ooops.</p>
+    <p>{{ error.message }}</p>
+    <button @click="handleClearError">Go Home...</button>
+  </div>
+</template>
+
+<script setup>
+  defineProps(['error']) 
+
+  const handleClearError = () => {
+    clearError({ redirect: '/' })
+  }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
--- pages/products/[id].vue
+++ pages/products/[id].vue
@@ -9,4 +9,12 @@
   const uri = "https://fakestoreapi.com/products/" + id
 
   const {data: product} = await useFetch(uri, {key:id})
+
+  if(!product) {
+    throw createError({statusCode: 404, statusMessage: 'Product not found', fatal: true})
+  }
+
+  definePageMeta({
+    layout: 'products',
+  })
 </script>
