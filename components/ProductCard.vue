4f998796bfc0d7ce6789b191893964bda838969b
Elbay Malik <27622683+EleoXDA@users.noreply.github.com>
2023-07-07 21:55:08 +0200

    prepare a reusable component for product card

---
+++ components/ProductCard.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <img :src="product.image" :alt="product.title" />
+    <p> {{ product.title }}</p>
+    <NuxtLink :to="`/products/${product.id}`">
+      <p>View Details</p>
+    </NuxtLink>
+  </div>
+</template>
+
+<script setup>
+  const { product } = defineProps(['product'])
+</script>
+
+<style scoped>
+
+</style>
--- pages/products/index.vue
+++ pages/products/index.vue
@@ -2,7 +2,7 @@
   <div>
     <div>
       <div v-for="p in products">
-        <NuxtLink :to="`/products/${p.id}`">{{p.title}}</NuxtLink>
+        <ProductCard :product="p" />
       </div>
     </div>
   </div>
