18aba5844a4622cd8fb804f731f5debf269fbe82
Elbay Malik <27622683+EleoXDA@users.noreply.github.com>
2023-07-07 17:39:16 +0200

    use FakeStore API to fetch items for the catalogue

--- pages/products/[id].vue
+++ pages/products/[id].vue
@@ -1,9 +1,14 @@
 <template>
   <div>
-    <h1>Product details for {{ id }}</h1>
+    <p>{{ product.title }}</p>
+    <p>{{ product.price }}</p>
+    <p>{{ product.id }}</p>
   </div>
 </template>
 
 <script setup>
-  const { id } = useRoute().params
+  const {id} = useRoute().params
+  const uri = "https://fakestoreapi.com/products/" + id
+
+  const {data: product} = await useFetch(uri, {key:id})
 </script>
