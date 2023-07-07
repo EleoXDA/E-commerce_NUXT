b01bff1d0d54fc0ff45efc7c9b45952d13bab0ff
Elbay Malik <27622683+EleoXDA@users.noreply.github.com>
2023-07-07 21:55:33 +0200

    prepare a reusable component for product details

---
+++ components/ProductDetails.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <img :src="product.image">
+    <p>{{ product.title }}</p>
+    <p>{{ product.price }}</p>
+    <p>{{ product.id }}</p>
+  </div>
+</template>
+
+<script setup>
+  const { product } = defineProps(['product'])
+</script>
+
+<style scoped>
+
+</style>
