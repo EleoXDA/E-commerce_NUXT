29852122685af87677a50fdc76efafdf43b07466
Elbay Malik <27622683+EleoXDA@users.noreply.github.com>
2023-07-07 17:39:02 +0200

    use FakeStore API to fetch items for the catalogue

--- pages/products/index.vue
+++ pages/products/index.vue
@@ -1,6 +1,10 @@
 <template>
   <div>
-    <h1>Products</h1>
+    <div>
+      <div v-for="p in products">
+        <NuxtLink :to="`/products/${p.id}`">{{p.title}}</NuxtLink>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -14,4 +18,6 @@
   definePageMeta({
     layout: 'products',
   })
+
+  const {data:products} = await useFetch('https://fakestoreapi.com/products')
 </script>
