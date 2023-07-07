33270da48bc7623ffebeb3191342fa7cf94889c3
Elbay Malik <27622683+EleoXDA@users.noreply.github.com>
2023-07-07 12:52:37 +0200

    default layout to replace repeated code

---
+++ layouts/default.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <header>
+      <nav>
+        <ul>
+          <li>
+            <NuxtLink to="/">Home</NuxtLink>
+          </li>
+          <li>
+            <NuxtLink to="/about">About</NuxtLink>
+          </li>
+          <li>
+            <NuxtLink to="/products">Products</NuxtLink>
+          </li>
+        </ul>
+      </nav>
+    </header>
+    <div>
+      <slot />
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    
+  }
+</script>
--- pages/index.vue
+++ pages/index.vue
@@ -1,20 +1,5 @@
 <template>
   <div>
-    <header>
-      <nav>
-        <ul>
-          <li>
-            <NuxtLink to="/">Home</NuxtLink>
-          </li>
-          <li>
-            <NuxtLink to="/about">About</NuxtLink>
-          </li>
-          <li>
-            <NuxtLink to="/products">Products</NuxtLink>
-          </li>
-        </ul>
-      </nav>
-    </header>
     <h1>Index</h1>
   </div>
 </template>
