9d20c33adaa3b364e1f871729e72959ab831a640
Elbay Malik <27622683+EleoXDA@users.noreply.github.com>
2023-07-08 10:51:08 +0200

    error: css +  code cleanup

--- error.vue
+++ error.vue
@@ -1,20 +1,16 @@
 <template>
-  <div>
-    <p>{{ error.statusCode }}</p>
-    <p>Ooops.</p>
-    <p>{{ error.message }}</p>
-    <button @click="handleClearError">Go Home...</button>
+  <div class="mt-7 max-w-sm mx-auto text-center card">
+    <p class="mt-7 text-7xl font-bold">{{error.statusCode}}</p>
+    <p class="mt-7 text-6xl">Ooops.</p>
+    <p class="mt-7">{{error.message}}</p>
+    <button class="btn my-7" @click="handleError">Go Home...</button>
   </div>
 </template>
 
 <script setup>
-  defineProps(['error']) 
+  defineProps({
+    error: Object
+  })
 
-  const handleClearError = () => {
-    clearError({ redirect: '/' })
-  }
+  const handleError = () => clearError({ redirect: '/' })
 </script>
-
-<style lang="scss" scoped>
-
-</style>
