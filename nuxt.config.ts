f3029ba5112ce1011ef324f70065ea6b33a5fe24
Elbay Malik <27622683+EleoXDA@users.noreply.github.com>
2023-07-08 11:24:49 +0200

    update main title and meta tag for the website

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,4 +3,15 @@ export default defineNuxtConfig({
   devtools: { enabled: true },
   modules: ['@nuxtjs/tailwindcss'],
   css: ['assets/css/main.css'],
+  app: {
+    head: {
+      title: 'E-Commerce',
+      meta: [
+        { name: 'E-Commerce Page', content: 'E-Commerce website' },
+      ],
+      link: [
+        { rel: 'stylesheet', href: 'https://fonts.googleapis.com/icon?family=Matierial+Icons' },
+      ]
+    }
+  }
 })
