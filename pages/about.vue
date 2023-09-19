aa96dc4a0ce8b68e5f3b8df68938ef3fd92dceca
Elbay Malik <27622683+EleoXDA@users.noreply.github.com>
2023-09-20 00:03:18 +0200

    add bg images

--- pages/about.vue
+++ pages/about.vue
@@ -1,7 +1,6 @@
 <template>
   <div>
-    <h1>About</h1>
-    <p>This is the about page</p>
+    <img src="../assets/images/about.jpg" />
   </div>
 </template>
 
--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <h1>Index</h1>
+    <img src="../assets/images/e-commerce bg.jpg" />
   </div>
 </template>
 
